fbfa49fc382cf5a9a600ce1c46aa95b5fb28a24a
constantindjonkam <fokouaconstantin@gmail.com>
2023-03-09 21:55:38 -0500

    added some colors and enabled dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3661FF",
+        secondary: "#000000",
+        dark: {
+          blue: "#060620",
+        },
+        light: {
+          test: "#ffffff",
+        },
+      },
+    },
   },
+  darkMode: "media",
   plugins: [],
-}
+};
