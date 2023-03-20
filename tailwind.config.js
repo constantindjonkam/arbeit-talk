68618ff1385e751bf7206907134647fd93ce15c7
constantindjonkam <fokouaconstantin@gmail.com>
2023-03-19 23:40:54 -0400

    added some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
         secondary: "",
         dark: {
           blue: "#060620",
-          grey: "#1A1C1E",
+          moon: "#374A70",
+          grey: { bg: "#1A1C1E", text: "#666666", input: "#2C3032", border: "#494F52" },
         },
         light: {
-          grey: "#FAFBFC",
+          blue: "",
+          moon: "#9AB3CD",
+          text: "#A8A095",
+          grey: { bg: "#FAFBFC" },
         },
       },
     },
