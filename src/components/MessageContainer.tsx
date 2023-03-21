0831990e2298807fc7ed62d23c1c1d7657a8dd63
constantindjonkam <fokouaconstantin@gmail.com>
2023-03-20 23:23:36 -0400

    display DiscussionList and MessageView

--- src/components/MessageContainer.tsx
+++ src/components/MessageContainer.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
+import DiscussionList from "./message/DiscussionList";
+import MessageView from "./message/MessageView";
 
 interface MessageContainerProps {}
 
 const MessageContainer: FC<MessageContainerProps> = ({}) => {
   return (
-    <div className="">
-      <h1 className="">Welcome To Arbeit Talk</h1>
+    <div className="flex p-4 h-[calc(100vh-56px)]">
+      <DiscussionList />
+      <MessageView />
     </div>
   );
 };
